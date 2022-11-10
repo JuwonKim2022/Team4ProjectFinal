a34791000f16e8e64797a786c8512ba442e4378c
DVallmoments <pub.allmoments@gmail.com>
2022-11-10 11:52:11 +0900

    2022-11-10 Commit

--- src/main/java/com/kh/component/RManager.java
+++ src/main/java/com/kh/component/RManager.java
@@ -6,62 +6,91 @@ import org.springframework.stereotype.Component;
 @Component
 public class RManager {
 	public void rGraph() {
+		System.out.println("rGraph의 R연결시도");
+		RConnection rc = null;
 		try {
-//        	System.out.println("rGraph의 R연결시도");
-//            RConnection rc = new RConnection();
-//            System.out.println("R연결완료");
-//            rc.voidEval("library(rJava)"); //Rserve에 R명령어 전송
-//            rc.voidEval("data<-read.csv(\"C:/Users/GangJu/Desktop/Spring3/workspace/MarketAnalysis2/src/main/webapp/resources/csv/r.csv\",header=T,sep=\",\")");
-//            rc.voidEval("png(\"C:/Users/GangJu/Desktop/Spring3/workspace/MarketAnalysis2/src/main/webapp/resources/png/rgp.png\")");
-//            rc.voidEval("barplot(data$marketquartersales/1000, names=data$marketyear, col=rainbow(15))");
-//            rc.voidEval("dev.off()");
-//            rc.close();
-			System.out.println("rGraph의 R연결시도");
-			RConnection rc = new RConnection();
+			rc = new RConnection();
 			System.out.println("R연결완료");
+			
 			rc.voidEval("library(rJava)");
-			rc.voidEval("library(Rserve)");
-			rc.voidEval("Rserve(args=\"--no-save\")");
-			rc.voidEval("library(ggplot2)");
-			rc.voidEval("data<-read.csv(\"C:/Users/GangJu/Desktop/Spring3/workspace/MarketAnalysis2/src/main/webapp/resources/csv/r.csv\",header=T,sep=\",\")");
+			
+			rc.voidEval("data<-read.csv(\"C:/Users/GangJu/Desktop/Spring3/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Team4MarketAnalysis/resources/csv/r.csv\",header=T,sep=\",\")");
+			System.out.println("CSV로부터 데이터 전달 완료");
 			
 			rc.voidEval("yq <- data[0:2]");
 			rc.voidEval("sumYQ <- paste(yq$marketyear, yq$marketquarter)");
 			rc.voidEval("marketquartersales <-data[3]");
 			rc.voidEval("marketquartercount <- data[4]");
 			rc.voidEval("marketofstores <- data[5]");
+			rc.voidEval("resultMQS <- cbind(sumYQ, marketquartersales)");
+			rc.voidEval("resultMQC <- cbind(sumYQ, marketquartercount)");
+			rc.voidEval("resultMOS <- cbind(sumYQ, marketofstores)");
 			
-			rc.voidEval("resultMS <- cbind(sumYQ, marketquartersales)");
-			rc.voidEval("resultMC <- cbind(sumYQ, marketquartercount)");
-			rc.voidEval("resultMS <- cbind(sumYQ, marketofstores)");
-			
-			rc.voidEval("png(\"C:/Users/GangJu/Desktop/Spring3/workspace/MarketAnalysis2/src/main/webapp/resources/png/taos.png\")");
-			rc.voidEval("ggplot(resultMS, aes(x=sumYQ, y=marketquartersales))+\r\n" + 
-						"geom_bar(stat=\"identity\")+\r\n" + 
-						"labs(title = \"연도 및 분기별 총 판매액\", x = \"Quarterly by Year\", y = \"Total Amount of Sales\")+\r\n" + 
-						"theme(axis.text.x = element_text(angle = 45, size = 15), plot.title = element_text(hjust = 0.5, size = 15))+\r\n" + 
-						"scale_y_continuous(labels = scales ::comma)");
+			rc.voidEval("png(\"C:/Users/GangJu/Desktop/Spring3/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Team4MarketAnalysis/resources/png/taos.png\")");
+			rc.voidEval("barplot(resultMQS$marketquartersales/10000, names.arg = resultMQS$sumYQ, main=\"Quarterly Sales\", cex.names = 0.7, xlab = \"Quarterly by Year\", ylab = \"Total Amount of Sales(Unit: 10,000won)\", las = 1, space = 1)");
 			rc.voidEval("dev.off()");
 			
-			rc.voidEval("png(\"C:/Users/GangJu/Desktop/Spring3/workspace/MarketAnalysis2/src/main/webapp/resources/png/tvos.png\")");
-			rc.voidEval("ggplot(resultMC, aes(x=sumYQ, y=marketquartercount))+\r\n" + 
-						"geom_bar(stat=\"identity\")+\r\n" + 
-						"labs(title = \"연도 및 분기별 총 판매수량\", x = \"Quarterly by Year\", y = \"Total Volume of Sales\")+\r\n" + 
-						"theme(axis.text.x = element_text(angle = 45, size = 15), plot.title = element_text(hjust = 0.5, size = 15))+\r\n" + 
-						"scale_y_continuous(labels = scales ::comma)");
+			rc.voidEval("png(\"C:/Users/GangJu/Desktop/Spring3/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Team4MarketAnalysis/resources/png/tvos.png\")");
+			rc.voidEval("barplot(resultMQC$marketquartercount/1000, names.arg = resultMQC$sumYQ, main=\"Quarterly Sales Volume\", cex.names = 0.7, xlab = \"Quarterly by Year\", ylab = \"Total Sales Volume(Unit: 1,000ea)\", las = 1, space = 1)");
 			rc.voidEval("dev.off()");
 			
-			rc.voidEval("png(\"C:/Users/GangJu/Desktop/Spring3/workspace/MarketAnalysis2/src/main/webapp/resources/png/tnos.png\")");
-			rc.voidEval("ggplot(resultMS, aes(x=sumYQ, y=marketofstores))+\r\n" + 
-						"geom_bar(stat=\"identity\")+\r\n" + 
-						"labs(title = \"연도 및 분기별 총 점포수\", x = \"Quarterly by Year\", y = \"Total Number of Stores\")+\r\n" + 
-						"theme(axis.text.x = element_text(angle = 45, size = 15), plot.title = element_text(hjust = 0.5, size = 15))+\r\n" + 
-						"scale_y_continuous(labels = scales ::comma)");
+			rc.voidEval("png(\"C:/Users/GangJu/Desktop/Spring3/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Team4MarketAnalysis/resources/png/tnos.png\")");
+			rc.voidEval("barplot(resultMOS$marketofstores, names.arg = resultMOS$sumYQ, main=\"Number Of Stores Per Quarter\", cex.names = 0.7, xlab = \"Quarterly by Year\", ylab = \"Total Number of Stores\", las = 1, space = 1)");
 			rc.voidEval("dev.off()");
 			
-			rc.close();
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			if(rc != null)
+				rc.close();
 		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
+		 * try { // rc.voidEval("library(Rserve)\r\n");
+		rc.voidEval("Rserve(args=\"--no-save\")");
+		rc.voidEval("Sys.setenv(JAVA_HOME='C:/Program Files/Java/jre1.8.0_333')");
+		rc.voidEval("library(RJDBC)"); rc.voidEval("library(DBI)");
+		rc.voidEval("library(rJava)"); rc.voidEval("library(ggplot2)");
+		System.out.println("R연결완료");
+
+		rc.voidEval("data<-read.csv(\"/resources/csv/r.csv\",header=T,sep=\",\")");
+ 
+		rc.voidEval("yq <- data[0:2]");
+		rc.voidEval("sumYQ <- paste(yq$marketyear, yq$marketquarter)");
+		rc.voidEval("marketquartersales <-data[3]");
+		rc.voidEval("marketquartercount <- data[4]");
+		rc.voidEval("marketofstores <- data[5]");
+
+		rc.voidEval("resultMQS <- cbind(sumYQ, marketquartersales)");
+		rc.voidEval("resultMQC <- cbind(sumYQ, marketquartercount)");
+		rc.voidEval("resultMOS <- cbind(sumYQ, marketofstores)");
+ 
+		rc.voidEval("png(\"/resources/png/taos.png\")");
+		rc.voidEval("barplot(resultMQS$marketquartersales/1000, names=resultMQS$sumYQ)");
+		rc.voidEval("dev.off()");
+	
+		rc.voidEval("png(\"/resources/png/tvos.png\")");
+		rc.voidEval("barplot(resultMQC$marketquartercount, names=resultMQC$sumYQ)");
+		rc.voidEval("dev.off()");
+		
+		rc.voidEval("png(\"/resources/png/tnos.png\")");
+		rc.voidEval("barplot(resultMOS$marketofstores, names=resultMOS$sumYQ)");
+		rc.voidEval("dev.off()");
+		
+		rc.close(); } catch (Exception e) { e.printStackTrace(); }
+		 */
 	}
 }
--- src/main/java/com/kh/controller/MarketController.java
+++ src/main/java/com/kh/controller/MarketController.java
@@ -1,9 +1,13 @@
 package com.kh.controller;
 
 import java.io.FileWriter;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,51 +18,100 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kh.component.RManager;
+import com.kh.service.HistoryService;
 import com.kh.service.MarketService;
+import com.kh.vo.HistoryDTO;
 import com.kh.vo.MarketDTO;
 
 @Controller
 public class MarketController {
+	boolean searchTrigger = false;
+
 	@Resource(name = "marketService")
 	private MarketService marketService;
-	
+
+	@Resource(name = "historyService")
+	private HistoryService historyService;
+
 	@Autowired
 	private RManager rm;
-	
+
 	@RequestMapping(value = "/MarketMapPage", method = RequestMethod.GET)
 	public String MapGet() {
 		return "MarketMapPage";
 	}
 
-	// 작동
+	// 작동: 검색 및 검색기록 조회 / 회원번호, 년도, 분기 설정 필요
 	@ResponseBody
-	@RequestMapping(value = "/MarketMapPage/map/search", method = RequestMethod.POST)
-	public List<MarketDTO> marketSearch(Model model, @RequestParam String bd_codename) throws Exception {
-		String returnDistrict = marketService.returnDistrict(bd_codename);
-		List<MarketDTO> marketList = marketService.selectDataByDYQ(returnDistrict);
-		model.addAttribute("marketList", marketList);
-		return marketList;
+	@RequestMapping(value = "/MarketMapPage/SearchInsertHistory", method = RequestMethod.POST)
+	public List<HistoryDTO> SearchInsertHistory(Model model, @RequestParam String searchText, HistoryDTO historyDTO) throws Exception {
+
+		int membernumber = 1;
+		int marketyear = 2021;
+		int marketquarter = 1;
+
+		if (searchText.contains("구")) {
+			historyDTO.setMembernumber(membernumber); // 회원번호
+			historyDTO.setMarketyear(marketyear); // 년도
+			historyDTO.setMarketquarter(marketquarter); // 분기
+			historyDTO.setDistrict(searchText);
+			historyDTO.setSearch_date(Timestamp.valueOf(LocalDateTime.now()));
+			historyService.insertSearchBox(historyDTO);
+		} else {
+			String returnDistrict = marketService.returnDistrict(searchText);
+			historyDTO.setMembernumber(membernumber); // 회원번호
+			historyDTO.setMarketyear(marketyear); // 년도
+			historyDTO.setMarketquarter(marketquarter); // 분기
+			historyDTO.setBd_codename(searchText);
+			historyDTO.setDistrict(returnDistrict);
+			historyDTO.setSearch_date(Timestamp.valueOf(LocalDateTime.now()));
+			historyService.insertSearchBox(historyDTO);
+		}
+
+		List<HistoryDTO> historyList = historyService.selectRecentHistory(membernumber);
+		model.addAttribute("historyList", historyList);
+		return historyList;
 	}
 
-	// 작동
+	// 작동: 정보 조회 / 회원번호, 년도, 분기 설정 필요
 	@ResponseBody
-	@RequestMapping(value = "/MarketMapPage/map/analysis", method = RequestMethod.POST)
-	public List<MarketDTO> marketAnalysis(Model model, @RequestParam String district) throws Exception {
-		List<MarketDTO> marketList = marketService.selectDataByDYQ(district);
-		model.addAttribute("marketList", marketList);
-		return marketList;
+	@RequestMapping(value = "/MarketMapPage/marketAnalysis", method = RequestMethod.POST)
+	public List<MarketDTO> marketAnalysis(Model model, @RequestParam String searchText) throws Exception {
+
+		if (searchText.contains("구")) {
+			List<MarketDTO> marketList = marketService.selectDataByDYQ(searchText);
+			model.addAttribute("marketList", marketList);
+			return marketList;
+		} else {
+			String returnDistrict = marketService.returnDistrict(searchText);
+			List<MarketDTO> marketList = marketService.selectDataByDYQ(returnDistrict);
+			model.addAttribute("marketList", marketList);
+			return marketList;
+		}
+
 	}
-	
+
 	@ResponseBody
-	@RequestMapping(value = "/RTest", method = RequestMethod.GET)
-	public String modal(Model model) throws Exception {
-		String district = "종로구";
+	@RequestMapping(value = "/RTest", method = RequestMethod.POST)
+	public void modal(HttpServletRequest request, @RequestParam String bd_codename) throws Exception {
+		ServletContext context = request.getSession().getServletContext();
+		String csvPath = context.getRealPath("/resources/csv/");
+		String district = null;
+		if (searchTrigger == true)
+			district = marketService.returnDistrict(bd_codename);
+		else
+			district = bd_codename;
+
+		System.out.println("*********************" + district + "*********************");
+
 		List<MarketDTO> marketList = marketService.selectRData(district);
 		String csv = "marketyear, marketquarter, marketquartersales, marketquartercount, marketofstores\n";
-		for(MarketDTO marketDTO : marketList)
-			csv += marketDTO.getMarketyear() + "," + marketDTO.getMarketquarter() + "," + marketDTO.getMarketquartersales() + "," + marketDTO.getMarketquartercount() + "," + marketDTO.getMarketofstores()+"\n";
+		for (MarketDTO marketDTO : marketList)
+			csv += marketDTO.getMarketyear() + "," + marketDTO.getMarketquarter() + ","
+					+ marketDTO.getMarketquartersales() + "," + marketDTO.getMarketquartercount() + ","
+					+ marketDTO.getMarketofstores() + "\n";
 		try {
-			FileWriter fw = new FileWriter("C:/Users/GangJu/Desktop/Spring3/workspace/MarketAnalysis2/src/main/webapp/resources/csv/r.csv");
+			FileWriter fw = new FileWriter(csvPath + "r.csv");
 			fw.write(csv);
 			fw.close();
 		} catch (Exception e) {
@@ -67,7 +120,5 @@ public class MarketController {
 		System.out.println("R연결 시도");
 		rm.rGraph();
 		System.out.println("R연결 종료");
-		model.addAttribute("marketList", marketList);
-		return "RTest";
 	}
 }
