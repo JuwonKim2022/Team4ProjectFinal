de8f910b58fa8bbeeb0ea06cc04cdd267352b965
wonmo <wonmo@DESKTOP-7DS9S9C>
2022-11-16 01:01:41 +0900

    내 정보 이동 컨트롤 추가

--- src/main/java/com/kh/controller/SignUpController.java
+++ src/main/java/com/kh/controller/SignUpController.java
@@ -1,6 +1,7 @@
 package com.kh.controller;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +12,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kh.vo.MemberVO;
+
 import com.kh.service.SignUpService;
 
 @Controller
@@ -162,4 +166,31 @@ public class SignUpController {
 			return "redirect:signUp/login";
 		}
 	}//end of loginPOST()
+	@GetMapping("/signUp/myInformation")
+	public String myInformation(MemberVO memberVO,HttpServletRequest req,Model model) throws Exception {
+		System.out.println("insertIdPostd");
+
+
+		HttpSession session = req.getSession();
+		
+	
+		 MemberVO userVO2= (MemberVO) session.getAttribute("id");
+/*
+			
+		 String e= userVO2.getEmail();
+		
+			String id = "asdasd2";
+		
+			List<UserVO> user = service.user(id);
+			if(!user.isEmpty()) {
+				for(Object value : user) {
+					System.out.println(value);
+				}
+			}
+	
+		System.out.println("***---***");
+			*/
+					return "/signUp/myInformation";
+	}
+	
 }
